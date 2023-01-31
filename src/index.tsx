12e1508087b7056cb9ce2c1f48a24f7b3c3a39d9
xing.guo <xing.guo@vixtel.com>
2023-01-31 18:04:56 +0800

    1:新增storybook

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
+
 import './index.less'
+import '@/assets/css/antd/index.less'
+import { DatePicker } from 'antd';
+import { Button } from 'antd';
 
 try {
     const container = document.getElementById('root');
     const root = createRoot(container);
     console.log("运行");
     const App = () => {
-        return <div className="hello">Hello</div>
+        return <div>
+            <DatePicker />
+            <Button type="primary">Primary Button</Button>
+        </div>;
     };
     root.render(<App />)
 } catch (e) {
